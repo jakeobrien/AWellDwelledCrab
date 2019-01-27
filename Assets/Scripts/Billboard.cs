42548f5a82a6b616d86ab1ee06b0aa066246d33a
Jake OBrien <neuronrain@gmail.com>
2019-01-26 21:07:15 -0500

    cinemachine cams, stuffs

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Billboard : MonoBehaviour
+{
+
+    public CameraReference cameraRef;
+    private Transform _cameraTransform;
+
+    private void Start()
+    {
+        _cameraTransform = cameraRef.value.transform;
+    }
+
+    private void Update()
+    {
+        var camRot = _cameraTransform.rotation;
+        transform.LookAt(transform.position + camRot * Vector3.forward, camRot * Vector3.up);
+    }
+
+}
