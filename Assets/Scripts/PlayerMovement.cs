d6ba0890f4d85784ecfcc5b5c0e3ae9290833754
Jake OBrien <neuronrain@gmail.com>
2019-01-27 04:19:18 -0500

    switched to character controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,13 @@ public class PlayerMovement : MonoBehaviour
     public float rotationNoiseSpeed;
     public PlayerState playerState;
     public CameraReference cameraReference;
-    public Rigidbody playerRigidbody;
+    public CharacterController controller;
     public GameObject destinationMarker;
 
     private Vector3? _destination;
     private Vector3 _velocityCursor;
     private Vector3 _direction;
+    private Vector3 _velocity;
 
     private void OnEnable()
     {
@@ -28,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (playerState.isInMenu)
         {
-            playerRigidbody.velocity = Vector3.zero;
+            _velocity = Vector3.zero;
             return;
         }
         GetInput();
@@ -46,13 +47,14 @@ public class PlayerMovement : MonoBehaviour
         targetVelocity += cameraReference.value.transform.right * _direction.x;
         targetVelocity.y = 0f;
         targetVelocity = targetVelocity.normalized * speed;
-        playerRigidbody.velocity = Vector3.SmoothDamp(playerRigidbody.velocity, targetVelocity, ref _velocityCursor, smoothing);
-        var targetRotation = Quaternion.LookRotation(playerRigidbody.transform.forward, Vector3.up);
-        if (playerRigidbody.velocity.sqrMagnitude > 1f) targetRotation = Quaternion.LookRotation(playerRigidbody.velocity.normalized, Vector3.up);
+        _velocity = Vector3.SmoothDamp(_velocity, targetVelocity, ref _velocityCursor, smoothing);
+        controller.SimpleMove(_velocity);
+        var targetRotation = Quaternion.LookRotation(controller.transform.forward, Vector3.up);
+        if (controller.velocity.sqrMagnitude > 1f) targetRotation = Quaternion.LookRotation(controller.velocity.normalized, Vector3.up);
         if (_direction.x != 0f) targetRotation = Quaternion.RotateTowards(targetRotation, Quaternion.LookRotation(cameraReference.value.transform.right * _direction.x, Vector3.up), 90f);
         // targetRotation *= Quaternion.AngleAxis(Mathf.PerlinNoise(Time.time * rotationNoiseSpeed, 0f) * rotationNoise, Vector3.up);
-        var rotation = Quaternion.RotateTowards(playerRigidbody.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        playerRigidbody.rotation = rotation;
+        var rotation = Quaternion.RotateTowards(controller.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        controller.transform.rotation = rotation;
     }
 
 }
